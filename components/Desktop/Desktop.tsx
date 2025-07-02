cca92d4cd1d7536df3c74ec777f3b383c0737192
ronis <ronis.tzol@gmail.com>
2025-07-02 21:20:02 +0100

    resolve conflict

--- components/Desktop/Desktop.tsx
+++ components/Desktop/Desktop.tsx
@@ -1,16 +1,12 @@
 'use client';
 
 import React from 'react';
-<<<<<<< HEAD
+
 import DesktopIcon from './components/DesktopIcon';
 import DesktopBackground from './components/DesktopBackground';
 import { useDesktop } from './hooks/useDesktop';
-=======
-import DesktopBackground from './DesktopBackground';
 import UnifiedItem from '@/components/Common/UnifiedItem';
 import { convertDesktopIconToUnified } from '@/components/Common/UnifiedItem/utils';
-import { useDesktop } from './useDesktop';
->>>>>>> origin/DragNDrop
 
 const Desktop: React.FC = () => {
   const {
